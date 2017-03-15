5c5eac9b691103310737aee8880b5c5249301645
Shane Sarnac <shane.sarnac@colorado.edu>
2017-03-15 01:27:29 -0600

    Completed 1a Implicit Eulers Plot

--- HW2/main.cpp
+++ HW2/main.cpp
@@ -5,113 +5,50 @@
 
 using namespace std;
 
-double y_prime(double x, double y) {
-	return -y;
+/* Problem 1 Begin */
+double realSolution(double t) {
+	return 3.0 - 0.998*exp(-1000.0*t) - 2.002*exp(-t);
 }
-
-double solutionPos(double x) {
-	return exp(x);
-}
-
-double solutionNeg(double x) {
-	return exp(-x);
-}
-
-void printSolutionPos(double x0, double xmax, double h) {
-	for (double i = x0; i < xmax; i += h) {
-		cout << "y(" << i << ") = " << solutionPos(i) << endl;
+void printSolution(double h, double t0, double tmax) {
+	cout << "Real Solution" << endl;
+	cout << "t, y" << endl;
+	for (double t = t0; t <= tmax; t += h) {
+		cout << t << "," << realSolution(t) << endl;
 	}
+	cout << endl;
 }
 
-void printSolutionNeg(double x0, double xmax, double h) {
-	for (double i = x0; i < xmax; i += h) {
-		cout << "y(" << i << ") = " << solutionNeg(i) << endl;
-	}
-}
-
-void printImplicitEulersHardcoded(double h, double A, double max, double x_0, double y_0) {
+void implicitEulers(double h, double t0, double y0, double tmax) {
+	double y_k, y_kp1, t_kp1, coeff;
+	int k = 0;
+	
 	cout << "Implicit Eulers" << endl;
-	double y_i, x_i = x_0;
+	cout << "k, t_k, y_k, y_real" << endl;
+	cout << k++ << "," << t0 << "," << y0 << "," << realSolution(t0) << endl;
 	
-	for (double i = 0; i < max; i++) {
-		y_i = y_0 * pow((1.0/(1.0 - A*h)), i); 
-		cout << "y(" << x_i << ") = " << y_i << endl;
-		x_i = x_i + h;
+	y_k = y0;
+	t_kp1 = t0 + h;
+	coeff = 1.0 / (1.0 + 1000.0*h);
+	
+	for (double t_k = t0; t_k < tmax; t_k += h) {
+		y_kp1 = coeff * (y_k + 3000.0*h - 2000*h*exp(-t_kp1));
+		cout << k++ << "," << t_kp1 << "," << y_kp1 << "," <<  realSolution(t_kp1) << endl;
+		y_k = y_kp1;
+		t_kp1 += h;
 	}
+	cout << endl;
 }
-
-void printLeapFrogHardcodedNeg(double h, double A, double max, double x_0, double y_0) {
-	cout << "Leap Frog " << endl;
-	double y_i, x_i = x_0;
-	
-	double c1 = 1.0 - (-1.0 + sqrt(2) - exp(-1.0))/(2.0*sqrt(2.0));
-	double c2 = (-1.0 + sqrt(2) - exp(-1.0))/(2.0*sqrt(2.0));
-	
-	
-	for (double i = 0; i < max; i++) {
-		y_i = c1*pow(A*h + sqrt(pow(A*h, 2.0) + 1.0), i) + c2*pow(A*h - sqrt(pow(A*h, 2.0) + 1.0), i);
-		cout << "y(" << x_i << ") = " << y_i << endl;
-		x_i = x_i + h;
-	}
-}
-
-void printLeapFrogHardcodedPos(double h, double A, double max, double x_0, double y_0) {
-	cout << "Leap Frog " << endl;
-	double y_i, x_i = x_0;
-	
-	double c2 = (1.0 + sqrt(2.0) - exp(1.0))/(2.0*sqrt(2.0));
-	
-	double c1 = 1.0 - c2;
-	
-	for (double i = 0; i < max; i++) {
-		y_i = c1*pow(A*h + sqrt(pow(A*h, 2.0) + 1.0), i) + c2*pow(A*h - sqrt(pow(A*h, 2.0) + 1.0), i);
-		cout << "y(" << x_i << ") = " << y_i << endl;
-		x_i = x_i + h;
-	}
-}
-
-void printRK2Hardcoded(double h, double A, double max, double x_0, double y_0) {
-	cout << "RK2 " << endl;
-	
-	double y_i, x_i = x_0; 
-	
-	for (double i = 0; i < max; i++) {
-		y_i = y_0*pow(1.0 + A*h + (0.5)*pow(A*h, 2.0), i);
-		cout << "y(" << x_i << ") = " << y_i << endl;
-		x_i = x_i + h;
-	}
-}
-
-void printMilnesHardcoded(double h, double A, double max, double x_0, double y_0) {
-	cout << "Milnes" << endl;
-	
-	double y_i, x_i = x_0;
-	double c1 = 1.0;
-	double c2 = 1.0;
-	
-	for (double i = 0; i < max; i++) {
-		y_i = c1*pow((2.0*A*h/3.0 + sqrt(3.0*pow(A*h/3.0, 2.0) + 1)*(1.0/(1.0 - A*h/3.0))), i);
-		y_i += c2*pow((2.0*A*h/3.0 - sqrt(3.0*pow(A*h/3.0, 2.0) + 1)*(1.0/(1.0 - A*h/3.0))), i);
-		cout << "y(" << x_i << ") = " << y_i << endl;
-		x_i = x_i + h;
-	}
-}
+/* Problem 1 End */
 
 int main() {
+	double h = 0.0001;
+	double t0 = 0;
+	double y0 = 0;
+	double tmax = 0.01;
 	
-	double h = 6.0;
-	double A = 1.0;
-	//printSolutionNeg(0.0, 15, h); cout << endl;
-	printSolutionPos(0.0, 15, h); 
-	//printLeapFrogHardcodedNeg(h, A, 20, 0.0, 1.0);
-	//printLeapFrogHardcodedPos(h, A, 20, 0.0, 1.0);
-
-	//improved_eulers(h, y_prime, 0.0, 15, 1.0);	
-	//printImplicitEulersHardcoded(h, A, 60, 0.0, 1.0);
+	implicitEulers(h, t0, y0, tmax);
 	
-	//printRK2Hardcoded(h, A, 20, 0.0, 1.0);
-	printMilnesHardcoded(h, A, 20, 0.0, 1.0);
-	
+	//printSolution(h, t0, tmax);
 	
 	return 0;
 }
