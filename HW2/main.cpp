6aaf7d370972ba5468c54126cfe209a6223d0830
Shane Sarnac <shane.sarnac@colorado.edu>
2017-03-15 02:14:53 -0600

    wrote outline for Gears 3 Point

--- HW2/main.cpp
+++ HW2/main.cpp
@@ -38,6 +38,32 @@ void implicitEulers(double h, double t0, double y0, double tmax) {
 	}
 	cout << endl;
 }
+
+void gearsThreePoint(double h, double t0, double y0, double t1, double y1, double t2, double y2, double tmax) {
+	double y_km2, y_km1, y_k, y_kp1, t_kp1, coeff;
+	
+	cout << "Gears Three Point" << endl;
+	cout << "t_k, y_k, y_real" << endl;
+	cout << t0 << "," << y0 << "," << realSolution(t0);
+	// How do we determine y1 and y2?
+	
+	y_km2 = y0;
+	y_km1 = y1;
+	y_k = y2;
+	t_kp1 = t2 + h;
+	coeff = 1.0 / (1.0 + 6000.0*h/11.0);
+	
+	for (double t_k = t2; t_k < tmax; t_k += h) {
+		y_kp1 = (1.0/11.0)* coeff * (2.0*y_km2 - 9.0*y_km1 + 18.0*y_k + 6.0*h*(3000.0 - 2000*exp(-t_kp1)));
+		cout << t_kp1 << "," << y_kp1 << "," <<  realSolution(t_kp1) << endl;
+		y_km2 = y_km1;
+		y_km1 = y_k;
+		y_k = y_kp1;
+		t_kp1 += h;
+	}
+	cout << endl;
+}
+
 /* Problem 1 End */
 
 int main() {
