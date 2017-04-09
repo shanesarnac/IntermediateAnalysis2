e656f47acf6b20772a235a9841a170cc407f9b42
Shane Sarnac <shane.sarnac@colorado.edu>
2017-04-09 08:29:48 -0600

    Cleared Main

--- HW4/main.cpp
+++ HW4/main.cpp
@@ -12,28 +12,8 @@ int main() {
 	
 	cout.setf(ios::fixed,ios::floatfield);
     cout.precision(3);
-    // Problem 1
-    
-    /**finiteDifferences();**/
-    
-    // Problem 2
-    /**
-	double delta_x = 1.0/8.0;
-	//double delta_t = 1.0/48.0; // makes r approx 0.5
-	double delta_t = 1.0/24; // makes r approx 1.0
-	//double w = 0.0;
-	//double w = 2.0/3.0;
-	//double w = 0.878;
-	double w = 1.0;
-	
-	generalMethod(delta_x, delta_t, initial_condition, w);
-	**/
-	
-	// Problem 3
-	/**problem3(); **/
-	
-	// Problem 4
-	problem4();
+
+	// Problem 1
 		
 	return 0;
 }
