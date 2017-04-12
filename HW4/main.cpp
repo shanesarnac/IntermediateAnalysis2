0980d758f9fbb0f88db130e34a07e8f623eb7c83
Shane Sarnac <shane.sarnac@colorado.edu>
2017-04-12 00:33:05 -0600

    intitial conditions for problem 6

--- HW4/main.cpp
+++ HW4/main.cpp
@@ -6,6 +6,33 @@
 
 using namespace std;
 
+#define PI 3.14159265359
+
+// Problem 6
+// Part a initial conditions
+double initialConditions6a (double x) {
+	return sin(2.0*PI*x);
+}
+
+// Part b initial conditions
+double intitialCondition6b (double x) {
+	if ((0 <= x) && (x <= 0.5)) {
+		return 2.0*x;
+	}
+	else if ((0.5 < x) && (x <= 1.0)) {
+		return 2.0 - 2.0*x;
+	}
+	else {
+		return 0.0;
+	}
+}
+
+void problem6(double ((*f)(double)) {
+	
+	
+}
+
+// Part b initial conditions
 
 
 int main() {
@@ -14,6 +41,9 @@ int main() {
     cout.precision(3);
 
 	// Problem 1
+	
+	// Problem 6
+	
 		
 	return 0;
 }
